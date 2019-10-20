e504e87d9b922d3dd95aca108e3c7237814aeea2
NeoWu1216 <neo1216.wu@gmail.com>
2019-10-20 17:27:23 -0500

    Finished baseline

--- backend.py
+++ backend.py
@@ -1,18 +1,33 @@
 from flask import Flask, request, render_template, jsonify
 import search
+import sys
+import os
+import time
+import threading
+import subprocess
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # restart_app()
     return render_template('index.html')
 
 @app.route("/query", methods=['POST'])
 def query():
     query_string = request.json["queryString"]
     print(query_string)
-    return jsonify([search.query(query_string)])
+    # restart_app()
+    output = subprocess.Popen(['python3' ,'search.py', query_string], stdout=subprocess.PIPE).stdout.read()
+    return jsonify([output.decode('utf8')])
 
+def restart_app_internal():
+    time.sleep(1)
+    os.execv(sys.executable, [sys.executable] + sys.argv)
+
+def restart_app(*items):
+    t = threading.Thread(target=restart_app_internal)
+    t.start()
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 8000, debug=False)
+    app.run(host = "0.0.0.0", port = 8000, threaded=True, debug=False)
--- search.py
+++ search.py
@@ -1,25 +1,35 @@
 import metapy
-import inspect
+# import importlib
 
-idx = metapy.index.make_inverted_index('main.toml')
-idx.num_docs()
-idx.avg_doc_length()
-ranker = metapy.index.OkapiBM25()
+
+# ranker = metapy.index.OkapiBM25()
 with open('./parsed_paper/parsed_paper.dat', "r", encoding='utf-8') as input_file:
     all_lines = input_file.read().split('\n')
 
 def query(string):
+    # importlib.reload(metapy)
+    idx = metapy.index.make_inverted_index('main.toml')
+    idx.num_docs()
+    idx.avg_doc_length()
+    # print('received')
+    ranker = metapy.index.OkapiBM25()
     q = metapy.index.Document()
     q.content(string) # query from AP news
-
+    # print('created query doc')
     top_docs = ranker.score(idx, q, num_results=5)
-
+    # print('found top 5')
     str_to_return = ""
     for num, (d_id, _) in enumerate(top_docs):
 #     print(idx.metadata(d_id).__dict__())
 #     print(inspect.getdoc(idx.metadata(d_id)))
-        content = idx.metadata(d_id)
+# content = idx.metadata(d_id)
         str_to_return+= all_lines[d_id]
         str_to_return += "<br>"
     print(str_to_return)
     return str_to_return
+
+
+if __name__ == '__main__':
+    import sys
+    print(sys.argv)
+    print(query(sys.argv[1]))
