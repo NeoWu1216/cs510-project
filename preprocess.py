1a9f68db682dfd09797c06ff8595f236d27119d0
smeng9 <38666763+smeng9@users.noreply.github.com>
2019-10-20 19:00:40 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,31 +1,33 @@
-import numpy as np
 import os
 import re
-import traceback
-## root path
-path = "./papers_to_index" 
-### file name ###
-files= os.listdir(path)
-# print(files)
-s = []
+
+# Path of unparsed papers
+path_unparsed = "./papers_to_index/"
+
+# Path of parsed papers
+path_parsed = "./parsed_paper/"
+
+# Content of all papers
+papers = []
+
+# Get files in the unparsed papers folder
+files = os.listdir(path_unparsed)
+
 for file in files:
-    if not os.path.isdir(file): #if folder or not 
-        with open(path+"/"+file, encoding="utf8") as f:
-            try:
-                text = f.read() # open
-            except:
-                traceback.print_exc()
-                print(f'file: {file}')
-            
+    # If file is not a folder
+    if not os.path.isdir(file):
+        with open(path_unparsed + file, encoding="utf8") as f:
+            text = f.read()
             res = re.findall('<title>(.*)</title>',text)[0]
             res += " "
             res += re.findall('<abstract>(.*)</abstract>',text)[0]
+            # If file is not empty
             if res.strip():
-                s.append(res) 
-print(len(s))
+                papers.append(res) 
 
-s = [' '.join(line.split('\n')) for line in s]
-s = '\n'.join(s)
+# Convert papers to MeTA format
+papers = [' '.join(line.split('\n')) for line in s]
+papers = '\n'.join(s)
 
-with open('parsed_paper/parsed_paper.dat', 'w', encoding="utf8") as f:
+with open(path_parsed + 'parsed_paper.dat', 'w', encoding="utf8") as f:
     f.write(s)
